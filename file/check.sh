3ef9fe1da5451b38094a54ece5ce86e70ef0ece6
EugeneNizhNov <82667098+EugeneNizhNov@users.noreply.github.com>
2024-05-18 21:18:31 +0300

    Update check.sh

--- file/check.sh
+++ file/check.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+server=192.168.56.10
+port=80
+filename="/var/www/html/index.nginx-debian.html"
+nc -vz $server $port
+chck=$(echo $?)
+if [ $chck -eq 0 ]; then
+ if  [ -f $filename ]; then
+#  echo Сервер $server, порт $port и файл $filename доступны"
+  exit 0
+ else
+#  echo Сервер $server, порт $port доступны, файл $filename недоступен"
+  exit 1
+ fi
+else
+ if [ -f $filename ]; then
+#  echo Сервер $server, порт $port недоступны, файл $filename доступен"
+  exit 1
+ else
+#  echo Сервер $server, порт $port недоступны, файл $filename недоступен"
+  exit 1
+ fi
+fi
